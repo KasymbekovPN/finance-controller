7fe6b07da627779da295d3ada0e6e0ea3bdd69a1
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-01-04 17:17:17 +0300

    User is done

---
+++ src/main/java/kpn/financecontroller/data/domain/User.java
@@ -0,0 +1,45 @@
+package kpn.financecontroller.data.domain;
+
+import kpn.financecontroller.data.entities.Role;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+public class User implements UserDetails {
+    @Getter
+    private String username;
+    @Getter
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/kpn/financecontroller/data/entities/Role.java
+++ src/main/java/kpn/financecontroller/data/entities/Role.java
@@ -1,5 +1,6 @@
 package kpn.financecontroller.data.entities;
 
+// TODO: 04.01.2022 move
 public enum Role {
     ROLE_ADMIN
 }
