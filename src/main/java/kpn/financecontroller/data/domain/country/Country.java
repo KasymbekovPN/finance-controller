f2b9627da1324c374d6b9f20f40fc50de2fa94f6
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-01-07 16:44:31 +0300

    Country is done

---
+++ src/main/java/kpn/financecontroller/data/domain/country/Country.java
@@ -0,0 +1,13 @@
+package kpn.financecontroller.data.domain.country;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Country {
+    private Long id;
+    private String name;
+}
