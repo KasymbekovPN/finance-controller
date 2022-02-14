fcf2c17df67cb1704011ab264671326f9171ba81
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-02-14 22:37:32 +0300

    AbstractLoadingTaskFactory is done

--- src/main/java/kpn/financecontroller/initialization/load/factories/CountryLoadingTaskFactory.java
+++ src/main/java/kpn/financecontroller/initialization/load/factories/CountryLoadingTaskFactory.java
@@ -3,31 +3,18 @@ package kpn.financecontroller.initialization.load.factories;
 import kpn.financecontroller.initialization.entities.CountryInitialEntity;
 import kpn.financecontroller.initialization.load.calculators.PathCalculator;
 import kpn.financecontroller.initialization.load.creators.CollectorCreator;
-import kpn.financecontroller.initialization.load.tasks.LoadingTask;
-import kpn.financecontroller.initialization.load.tasks.LoadingTaskImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-// TODO: 14.02.2022 add base class
 @Service
 @Profile("dev")
-public class CountryLoadingTaskFactory implements LoadingTaskFactory<Long, CountryInitialEntity> {
+public class CountryLoadingTaskFactory extends AbstractLoadingTaskFactory<Long, CountryInitialEntity> {
 
     private static final String PATH = "countries.json";
     private static final String ID = "COUNTRIES";
 
-    private final PathCalculator<String, String> pathCalculator;
-    private final CollectorCreator<Long, CountryInitialEntity> collectorCreator;
-
-    @Autowired
-     public CountryLoadingTaskFactory(PathCalculator<String, String> pathCalculator, CollectorCreator<Long, CountryInitialEntity> collectorCreator) {
-          this.pathCalculator = pathCalculator;
-          this.collectorCreator = collectorCreator;
-     }
-
-     @Override
-     public LoadingTask<Long, CountryInitialEntity> create() {
-          return new LoadingTaskImpl<>(pathCalculator, collectorCreator, PATH, ID);
-     }
+    public CountryLoadingTaskFactory(PathCalculator<String, String> pathCalculator,
+                                     CollectorCreator<Long, CountryInitialEntity> collectorCreator) {
+        super(pathCalculator, collectorCreator, ID, PATH);
+    }
 }
--- src/main/java/kpn/financecontroller/initialization/load/factories/TagLoadingTaskFactory.java
+++ src/main/java/kpn/financecontroller/initialization/load/factories/TagLoadingTaskFactory.java
@@ -3,30 +3,20 @@ package kpn.financecontroller.initialization.load.factories;
 import kpn.financecontroller.initialization.load.calculators.PathCalculator;
 import kpn.financecontroller.initialization.load.creators.CollectorCreator;
 import kpn.financecontroller.initialization.entities.TagInitialEntity;
-import kpn.financecontroller.initialization.load.tasks.LoadingTaskImpl;
-import kpn.financecontroller.initialization.load.tasks.LoadingTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile("dev")
-public class TagLoadingTaskFactory implements LoadingTaskFactory<Long, TagInitialEntity> {
+public class TagLoadingTaskFactory extends AbstractLoadingTaskFactory<Long, TagInitialEntity> {
 
     private static final String PATH = "tags.json";
     private static final String ID = "TAGS";
 
-    private final PathCalculator<String, String> pathCalculator;
-    private final CollectorCreator<Long, TagInitialEntity> collectorCreator;
-
     @Autowired
-    public TagLoadingTaskFactory(PathCalculator<String, String> pathCalculator, CollectorCreator<Long, TagInitialEntity> collectorCreator) {
-        this.pathCalculator = pathCalculator;
-        this.collectorCreator = collectorCreator;
-    }
-
-    @Override
-    public LoadingTask<Long, TagInitialEntity> create() {
-        return new LoadingTaskImpl<>(pathCalculator, collectorCreator, PATH, ID);
+    public TagLoadingTaskFactory(PathCalculator<String, String> pathCalculator,
+                                 CollectorCreator<Long, TagInitialEntity> collectorCreator) {
+        super(pathCalculator, collectorCreator, ID, PATH);
     }
 }
