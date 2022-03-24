3266add39ac51f940e1828fdae5683e3a5740ec1
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-03-24 22:54:55 +0300

    ReadingTaskSeedGenerator is done

---
+++ src/main/java/kpn/financecontroller/initialization/generators/seed/ReadingTaskSeedGenerator.java
@@ -0,0 +1,107 @@
+package kpn.financecontroller.initialization.generators.seed;
+
+import kpn.financecontroller.initialization.generators.valued.Valued;
+import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
+import kpn.financecontroller.initialization.managers.context.ResultContextManager;
+import kpn.financecontroller.initialization.tasks.FileReadingTask;
+import kpn.taskexecutor.lib.contexts.Context;
+import kpn.taskexecutor.lib.generators.Generator;
+import kpn.taskexecutor.lib.seeds.Seed;
+import kpn.taskexecutor.lib.seeds.SeedImpl;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Slf4j
+public class ReadingTaskSeedGenerator implements Generator {
+
+    private final Deque<PathItem> pathItems;
+    private final ValuedGenerator<String> valuedGenerator;
+    private final Function<Context, ResultContextManager> managerCreator;
+
+    private Boolean fieldValid = null;
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    private ReadingTaskSeedGenerator(Deque<PathItem> pathItems,
+                                     ValuedGenerator<String> valuedGenerator,
+                                     Function<Context, ResultContextManager> managerCreator) {
+        this.pathItems = pathItems;
+        this.valuedGenerator = valuedGenerator;
+        this.managerCreator = managerCreator;
+    }
+
+    @Override
+    public Optional<Seed> getNextIfExist(Context context) {
+        if (checkFields()){
+            PathItem pathItem = pathItems.pollFirst();
+            if (pathItem != null){
+                Seed seed = new SeedImpl.Builder()
+                        .type(FileReadingTask.class)
+                        .field("valuedGenerator", valuedGenerator)
+                        .field("managerCreator", managerCreator)
+                        .field("path", pathItem.getPath())
+                        .field("key", pathItem.getKey())
+                        .build();
+                return Optional.of(seed);
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean checkFields() {
+        if (fieldValid == null){
+            if (managerCreator == null){
+                log.warn("ManagerCreator is null");
+                fieldValid = false;
+                return false;
+            }
+            if (valuedGenerator == null){
+                log.warn("ValuedGenerator is null");
+                fieldValid = false;
+                return false;
+            }
+            fieldValid = true;
+        }
+        return fieldValid;
+    }
+
+    public static class Builder{
+        private final Deque<PathItem> pathItems = new ArrayDeque<>();
+        private ValuedGenerator<String> valuedGenerator;
+        private Function<Context, ResultContextManager> managerCreator;
+
+        public Builder setValuedGenerator(ValuedGenerator<String> valuedGenerator){
+            this.valuedGenerator = valuedGenerator;
+            return this;
+        }
+
+        public Builder setManagerCreator(Function<Context, ResultContextManager> managerCreator){
+            this.managerCreator = managerCreator;
+            return this;
+        }
+
+        public Builder addPathItem(Valued<String> key, String path){
+            pathItems.addLast(new PathItem(key, path));
+            return this;
+        }
+
+        public ReadingTaskSeedGenerator build(){
+            return new ReadingTaskSeedGenerator(pathItems, valuedGenerator, managerCreator);
+        }
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    private static class PathItem{
+        private final Valued<String> key;
+        private final String path;
+    }
+}
---
+++ src/test/java/kpn/financecontroller/initialization/generators/seed/ReadingTaskSeedGeneratorTest.java
@@ -0,0 +1,69 @@
+package kpn.financecontroller.initialization.generators.seed;
+
+import kpn.financecontroller.initialization.generators.valued.Valued;
+import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
+import kpn.financecontroller.initialization.generators.valued.ValuedStringGenerator;
+import kpn.financecontroller.initialization.managers.context.ResultContextManager;
+import kpn.financecontroller.initialization.tasks.FileReadingTask;
+import kpn.financecontroller.initialization.tasks.testUtils.TestKeys;
+import kpn.financecontroller.initialization.tasks.testUtils.TestManagerCreator;
+import kpn.taskexecutor.lib.contexts.Context;
+import kpn.taskexecutor.lib.contexts.SimpleContext;
+import kpn.taskexecutor.lib.seeds.Seed;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReadingTaskSeedGeneratorTest {
+
+    private static final Context CONTEXT = new SimpleContext();
+    private static final Function<Context, ResultContextManager> CREATOR = new TestManagerCreator();
+    private static final ValuedGenerator<String> VALUED_GENERATOR = new ValuedStringGenerator();
+    private static final Valued<String> KEY = TestKeys.KEY;
+    private static final String PATH = "path";
+
+    @Test
+    void shouldCheckNextGetting_ifManagerCreatorNull() {
+        ReadingTaskSeedGenerator seedGenerator = ReadingTaskSeedGenerator.builder().build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifValuedGeneratorNull() {
+        ReadingTaskSeedGenerator seedGenerator = ReadingTaskSeedGenerator.builder()
+                .setManagerCreator(CREATOR)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting() {
+        Map<String, Object> expectedFields = Map.of(
+                "valuedGenerator", VALUED_GENERATOR,
+                "managerCreator", CREATOR,
+                "path", PATH,
+                "key", KEY
+        );
+
+        ReadingTaskSeedGenerator seedGenerator = ReadingTaskSeedGenerator.builder()
+                .setManagerCreator(CREATOR)
+                .setValuedGenerator(VALUED_GENERATOR)
+                .addPathItem(KEY, PATH)
+                .build();
+
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isPresent();
+        Seed seed = maybeSeed.get();
+        assertThat(seed.getType()).isEqualTo(FileReadingTask.class);
+        assertThat(seed.getFields()).isEqualTo(expectedFields);
+
+        maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+}
