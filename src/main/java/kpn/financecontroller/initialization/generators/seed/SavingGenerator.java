c13b50b08a70ba9aebfee38280119f5b475959fb
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-03-28 22:22:36 +0300

    SavingGenerator is done

--- src/main/java/kpn/financecontroller/initialization/generators/seed/SavingGenerator.java
+++ src/main/java/kpn/financecontroller/initialization/generators/seed/SavingGenerator.java
@@ -1,47 +1,50 @@
 package kpn.financecontroller.initialization.generators.seed;
 
+import kpn.financecontroller.data.services.DTOService;
 import kpn.financecontroller.initialization.generators.valued.Properties;
 import kpn.financecontroller.initialization.generators.valued.Valued;
 import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
-import kpn.financecontroller.initialization.jsonObjs.TagLongKeyJsonObj;
 import kpn.financecontroller.initialization.managers.context.ResultContextManager;
 import kpn.financecontroller.result.Result;
 import kpn.taskexecutor.lib.contexts.Context;
 import kpn.taskexecutor.lib.generators.Generator;
 import kpn.taskexecutor.lib.seeds.Seed;
 import kpn.taskexecutor.lib.seeds.SeedImpl;
-import kpn.taskexecutor.lib.tasks.Task;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.config.Task;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 
 @Slf4j
-final public class ConversionGenerator implements Generator {
+final public class SavingGenerator implements Generator {
+
     private final ValuedGenerator<String> valuedGenerator;
     private final Function<Context, ResultContextManager> managerCreator;
     private final Valued<String> key;
     private final Class<? extends Task> type;
-
-    private Boolean fieldValidity;
-    private Deque<Long> entityIds;
+    private final DTOService<?, ?, Long> dtoService;
+    private final Class<? extends Map<Long, ?>> storageType;
 
     public static Builder builder(){
         return new Builder();
     }
 
-    private ConversionGenerator(ValuedGenerator<String> valuedGenerator,
-                                Function<Context, ResultContextManager> managerCreator,
-                                Valued<String> key,
-                                Class<? extends Task> type) {
+    private SavingGenerator(ValuedGenerator<String> valuedGenerator,
+                            Function<Context, ResultContextManager> managerCreator,
+                            Valued<String> key, Class<? extends Task> type,
+                            DTOService<?, ?, Long> dtoService,
+                            Class<? extends Map<Long, ?>> storageType) {
         this.valuedGenerator = valuedGenerator;
         this.managerCreator = managerCreator;
         this.key = key;
         this.type = type;
+        this.dtoService = dtoService;
+        this.storageType = storageType;
     }
+
+    private Boolean fieldValidity;
+    private Deque<Long> entityIds;
 
     @Override
     public Optional<Seed> getNextIfExist(Context context) {
@@ -52,6 +55,8 @@ final public class ConversionGenerator implements Generator {
                         .type(type)
                         .field("managerCreator", managerCreator)
                         .field("valuedGenerator", valuedGenerator)
+                        .field("key", key)
+                        .field("dtoService", dtoService)
                         .field("entityId", maybeId.get())
                         .build();
                 return Optional.of(seed);
@@ -60,19 +65,26 @@ final public class ConversionGenerator implements Generator {
         return Optional.empty();
     }
 
+    // TODO: 28.03.2022 optimize it - to builder-like inner class
     private boolean checkFields() {
         if (fieldValidity == null){
             if (managerCreator == null){
-                log.warn("Manager creator is null");
+                log.info("Manager creator is null");
                 fieldValidity = false;
-            } else if(valuedGenerator == null) {
-                log.warn("Valued generator is null");
+            } else if (valuedGenerator == null){
+                log.info("Valued generator is null");
                 fieldValidity = false;
             } else if (key == null){
-                log.warn("Key is null");
+                log.info("Key is null");
                 fieldValidity = false;
             } else if (type == null){
-                log.warn("Type is null");
+                log.info("Type is null");
+                fieldValidity = false;
+            } else if (dtoService == null){
+                log.info("DTO service is null");
+                fieldValidity = false;
+            } else if (storageType == null){
+                log.info("Storage type is null");
                 fieldValidity = false;
             } else {
                 fieldValidity = true;
@@ -83,10 +95,9 @@ final public class ConversionGenerator implements Generator {
 
     private Optional<Long> getNextEntityId(Context context) {
         if (entityIds == null){
-            // TODO: 28.03.2022 ??? TagLongKeyJsonObj.class
-            Result<TagLongKeyJsonObj> result = managerCreator.apply(context).get(key, Properties.JSON_OBJECT_CREATION_RESULT, TagLongKeyJsonObj.class);
+            Result<Object> result = managerCreator.apply(context).get(key, Properties.JSON_TO_DB_CONVERSION_RESULT);
             if (result.getSuccess()){
-                Set<Long> ids = result.getValue().getEntities().keySet();
+                Set<Long> ids = storageType.cast(result.getValue()).keySet();
                 entityIds = new ArrayDeque<>(ids);
                 Long id = entityIds.pollFirst();
                 if (id != null){
@@ -102,29 +113,41 @@ final public class ConversionGenerator implements Generator {
         private Function<Context, ResultContextManager> managerCreator;
         private Valued<String> key;
         private Class<? extends Task> type;
+        private DTOService<?, ?, Long> dtoService;
+        private Class<? extends Map<Long, ?>> storageType;
 
-        public Builder valuedGenerator(ValuedGenerator<String> valuedGenerator){
-            this.valuedGenerator = valuedGenerator;
-            return this;
-        }
-
-        public Builder managerCreator(Function<Context, ResultContextManager> managerCreator){
+        public Builder managerCreator(Function<Context, ResultContextManager> managerCreator) {
             this.managerCreator = managerCreator;
             return this;
         }
 
-        public Builder key(Valued<String> key){
+        public Builder valuedGenerator(ValuedGenerator<String> valuedGenerator) {
+            this.valuedGenerator = valuedGenerator;
+            return this;
+        }
+
+        public Builder key(Valued<String> key) {
             this.key = key;
             return this;
         }
 
-        public Builder type(Class<? extends Task> type){
+        public Builder type(Class<? extends Task> type) {
             this.type = type;
             return this;
         }
 
-        public ConversionGenerator build(){
-            return new ConversionGenerator(valuedGenerator, managerCreator, key, type);
+        public Builder dtoService(DTOService<?, ?, Long> dtoService) {
+            this.dtoService = dtoService;
+            return this;
+        }
+
+        public Builder storageType(Class<? extends Map<Long, ?>> storageType) {
+            this.storageType = storageType;
+            return this;
+        }
+
+        public SavingGenerator build(){
+            return new SavingGenerator(valuedGenerator, managerCreator, key, type, dtoService, storageType);
         }
     }
 }
---
+++ src/test/java/kpn/financecontroller/initialization/generators/seed/SavingGeneratorTest.java
@@ -0,0 +1,160 @@
+package kpn.financecontroller.initialization.generators.seed;
+
+import kpn.financecontroller.data.entities.tag.TagEntity;
+import kpn.financecontroller.data.services.DTOService;
+import kpn.financecontroller.initialization.generators.valued.Properties;
+import kpn.financecontroller.initialization.generators.valued.Valued;
+import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
+import kpn.financecontroller.initialization.generators.valued.ValuedStringGenerator;
+import kpn.financecontroller.initialization.managers.context.ResultContextManager;
+import kpn.financecontroller.initialization.storages.TagStorage;
+import kpn.financecontroller.initialization.tasks.testUtils.TestKeys;
+import kpn.financecontroller.initialization.tasks.testUtils.TestManagerCreator;
+import kpn.financecontroller.result.Result;
+import kpn.taskexecutor.lib.contexts.Context;
+import kpn.taskexecutor.lib.contexts.SimpleContext;
+import kpn.taskexecutor.lib.seeds.Seed;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.scheduling.config.Task;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SavingGeneratorTest {
+
+    private static final Context CONTEXT = new SimpleContext();
+    private static final Function<Context, ResultContextManager> CREATOR = new TestManagerCreator();
+    private static final ValuedGenerator<String> VALUED_GENERATOR = new ValuedStringGenerator();
+    private static final Valued<String> KEY = TestKeys.KEY;
+    private static final Class<? extends Task> TYPE = Task.class;
+    private static final Long ENTITY_ID = 1L;
+    private static final Class<? extends Map<Long, ?>> STORAGE_TYPE = TagStorage.class;
+
+    @Test
+    void shouldCheckNextGetting_ifManagerCreatorNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder().build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifValuedGeneratorNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifKeyNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifTypeNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .key(KEY)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifDTOServiceNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .key(KEY)
+                .type(TYPE)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifStorageTypeNull() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .key(KEY)
+                .type(TYPE)
+                .dtoService(createDTOService())
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifConversionResultAbsent() {
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .key(KEY)
+                .type(TYPE)
+                .dtoService(createDTOService())
+                .storageType(STORAGE_TYPE)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting() {
+        DTOService<?, ?, Long> dtoService = createDTOService();
+        Map<String, Object> expectedFields = Map.of(
+                "valuedGenerator", VALUED_GENERATOR,
+                "managerCreator", CREATOR,
+                "key", KEY,
+                "dtoService", dtoService,
+                "entityId", ENTITY_ID
+        );
+
+        TagEntity tagEntity = new TagEntity();
+        tagEntity.setId(ENTITY_ID);
+        tagEntity.setName("name");
+
+        TagStorage tagStorage = new TagStorage();
+        tagStorage.put(ENTITY_ID, tagEntity);
+
+        Result<TagStorage> result = Result.<TagStorage>builder()
+                .success(true)
+                .value(tagStorage)
+                .build();
+        CREATOR.apply(CONTEXT).put(KEY, Properties.JSON_TO_DB_CONVERSION_RESULT, result);
+
+        SavingGenerator seedGenerator = SavingGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
+                .key(KEY)
+                .type(TYPE)
+                .dtoService(dtoService)
+                .storageType(STORAGE_TYPE)
+                .build();
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isPresent();
+        Seed seed = maybeSeed.get();
+        assertThat(seed.getType()).isEqualTo(TYPE);
+        assertThat(seed.getFields()).isEqualTo(expectedFields);
+
+        maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    private DTOService<?, ?, Long> createDTOService() {
+        return Mockito.mock(CleanupGeneratorTest.TestDTOService.class);
+    }
+
+    public abstract static class TestDTOService implements DTOService<String, String, Long> {}
+}
