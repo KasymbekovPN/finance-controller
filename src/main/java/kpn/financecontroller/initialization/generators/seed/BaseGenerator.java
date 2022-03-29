fb523e58ef486fe6048e1cf0e367ad7982da9b4d
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-03-29 23:01:40 +0300

    CleanupGenerator refactoring is done

--- src/main/java/kpn/financecontroller/initialization/generators/seed/BaseGenerator.java
+++ src/main/java/kpn/financecontroller/initialization/generators/seed/BaseGenerator.java
@@ -5,15 +5,18 @@ import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
 import kpn.financecontroller.initialization.managers.context.ResultContextManager;
 import kpn.taskexecutor.lib.contexts.Context;
 import kpn.taskexecutor.lib.generators.Generator;
+import kpn.taskexecutor.lib.seeds.Seed;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Optional;
 import java.util.function.Function;
 
 public abstract class BaseGenerator implements Generator {
+    protected final FieldValidator fieldValidator = new FieldValidator();
     protected final Valued<String> key;
     protected final ValuedGenerator<String> valuedGenerator;
     protected final Function<Context, ResultContextManager> managerCreator;
@@ -24,7 +27,26 @@ public abstract class BaseGenerator implements Generator {
         this.key = key;
         this.valuedGenerator = valuedGenerator;
         this.managerCreator = managerCreator;
+
+        fieldValidator
+                .toChecking(managerCreator, "Manager creator")
+                .toChecking(valuedGenerator, "Valued generator")
+                .toChecking(key, "Key");
     }
+
+    @Override
+    public Optional<Seed> getNextIfExist(Context context) {
+        return checkFields() ? getNext(context) : Optional.empty();
+    }
+
+    private boolean checkFields() {
+        if (fieldValidator.isNotDone()){
+            fieldValidator.validate();
+        }
+        return fieldValidator.isValid();
+    }
+
+    protected abstract Optional<Seed> getNext(Context context);
 
     public abstract static class BaseBuilder {
         protected Valued<String> key;
@@ -50,29 +72,35 @@ public abstract class BaseGenerator implements Generator {
     }
 
     @Slf4j
-    @RequiredArgsConstructor
     protected static class FieldValidator{
         private final Deque<Item> items = new ArrayDeque<>();
-        private final Class<?> caller;
 
         @Getter
-        private boolean done;
+        private boolean notDone = true;
         @Getter
         private boolean valid;
+        private String caller = "";
 
-        public FieldValidator addToChecking(Object object, String mark){
-            items.addLast(new Item(object, mark));
+        public FieldValidator caller(String caller){
+            this.caller = caller;
+            return this;
+        }
+
+        public FieldValidator toChecking(Object object, String mark){
+            items.add(new Item(object, mark));
             return this;
         }
 
         public FieldValidator validate(){
-            done = valid = true;
+            notDone = false;
+            valid = true;
             do {
                 Item item = items.pollFirst();
                 if (item != null && item.getObject() == null){
-                    log.warn("{} : {} is null", caller.getSimpleName(), item.getMark());
+                    valid = false;
+                    log.warn("{} : {} is null", caller, item.getMark());
                 }
-            } while (items.size() == 0);
+            } while (items.size() != 0);
             return this;
         }
 
--- src/main/java/kpn/financecontroller/initialization/generators/seed/CleanupGenerator.java
+++ src/main/java/kpn/financecontroller/initialization/generators/seed/CleanupGenerator.java
@@ -1,6 +1,7 @@
 package kpn.financecontroller.initialization.generators.seed;
 
 import kpn.financecontroller.data.services.DTOService;
+import kpn.financecontroller.initialization.generators.valued.Valued;
 import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
 import kpn.financecontroller.initialization.managers.context.ResultContextManager;
 import kpn.financecontroller.initialization.tasks.CleanupTask;
@@ -8,86 +9,55 @@ import kpn.taskexecutor.lib.contexts.Context;
 import kpn.taskexecutor.lib.generators.Generator;
 import kpn.taskexecutor.lib.seeds.Seed;
 import kpn.taskexecutor.lib.seeds.SeedImpl;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 import java.util.function.Function;
 
-@Slf4j
-final public class CleanupGenerator implements Generator {
-    private final ValuedGenerator<String> valuedGenerator;
-    private final Function<Context, ResultContextManager> managerCreator;
-    private final Deque<DTOService<?, ?, Long>> dtoServices;
+final public class CleanupGenerator extends BaseGenerator {
 
-    private Boolean fieldValidity;
+    private final Deque<DTOService<?, ?, Long>> dtoServices;
 
     public static Builder builder(){
         return new Builder();
     }
 
-    public CleanupGenerator(ValuedGenerator<String> valuedGenerator,
-                            Function<Context, ResultContextManager> managerCreator,
-                            List<DTOService<?, ?, Long>> dtoServices) {
-        this.valuedGenerator = valuedGenerator;
-        this.managerCreator = managerCreator;
+    private CleanupGenerator(Valued<String> key,
+                             ValuedGenerator<String> valuedGenerator,
+                             Function<Context, ResultContextManager> managerCreator,
+                             List<DTOService<?, ?, Long>> dtoServices) {
+        super(key, valuedGenerator, managerCreator);
         this.dtoServices = new ArrayDeque<>(dtoServices);
+
+        this.fieldValidator.caller(getClass().getSimpleName());
     }
 
     @Override
-    public Optional<Seed> getNextIfExist(Context context) {
-        if (checkFields()){
-            DTOService<?, ?, Long> dtoService = dtoServices.pollFirst();
-            if (dtoService != null){
-                Seed seed = SeedImpl.builder()
-                        .type(CleanupTask.class)
-                        .field("managerCreator", managerCreator)
-                        .field("valuedGenerator", valuedGenerator)
-                        .field("dtoService", dtoService)
-                        .build();
-                return Optional.of(seed);
-            }
+    protected Optional<Seed> getNext(Context context) {
+        DTOService<?, ?, Long> dtoService = dtoServices.pollFirst();
+        if (dtoService != null){
+            Seed seed = SeedImpl.builder()
+                    .type(CleanupTask.class)
+                    .field("managerCreator", managerCreator)
+                    .field("valuedGenerator", valuedGenerator)
+                    .field("key", key)
+                    .field("dtoService", dtoService)
+                    .build();
+            return Optional.of(seed);
         }
         return Optional.empty();
     }
 
-    private boolean checkFields() {
-        if (fieldValidity == null){
-            if (managerCreator == null){
-                log.warn("Manager creator is null");
-                fieldValidity = false;
-            } else if (valuedGenerator == null){
-                log.warn("Valued generator is null");
-                fieldValidity = false;
-            } else {
-                fieldValidity = true;
-            }
-        }
-        return fieldValidity;
-    }
-
-    public static class Builder {
+    public static class Builder extends BaseBuilder{
         private final List<DTOService<?, ?, Long>> dtoServices = new ArrayList<>();
-
-        private ValuedGenerator<String> valuedGenerator;
-        private Function<Context, ResultContextManager> managerCreator;
-
-        public Builder managerCreator(Function<Context, ResultContextManager> managerCreator){
-            this.managerCreator = managerCreator;
-            return this;
-        }
-
-        public Builder valuedGenerator(ValuedGenerator<String> valuedGenerator){
-            this.valuedGenerator = valuedGenerator;
-            return this;
-        }
 
         public Builder dtoService(DTOService<?, ?, Long> type){
             dtoServices.add(type);
             return this;
         }
 
-        public CleanupGenerator build(){
-            return new CleanupGenerator(valuedGenerator, managerCreator, dtoServices);
+        @Override
+        public Generator build() {
+            return new CleanupGenerator(key, valuedGenerator, managerCreator, dtoServices);
         }
     }
 }
