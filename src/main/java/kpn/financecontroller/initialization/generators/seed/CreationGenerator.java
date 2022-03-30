e73a1ae5f8d6fa79ac90bfdb90585cc563e06968
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-03-30 22:09:22 +0300

    Refactoring of CreationGenerator

--- src/main/java/kpn/financecontroller/initialization/generators/seed/CreationGenerator.java
+++ src/main/java/kpn/financecontroller/initialization/generators/seed/CreationGenerator.java
@@ -1,93 +1,61 @@
 package kpn.financecontroller.initialization.generators.seed;
 
+import kpn.financecontroller.initialization.generators.valued.Valued;
 import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
 import kpn.financecontroller.initialization.jsonObjs.LongKeyJsonObj;
 import kpn.financecontroller.initialization.managers.context.ResultContextManager;
 import kpn.financecontroller.initialization.tasks.CreationTask;
 import kpn.taskexecutor.lib.contexts.Context;
-import kpn.taskexecutor.lib.generators.Generator;
 import kpn.taskexecutor.lib.seeds.Seed;
 import kpn.taskexecutor.lib.seeds.SeedImpl;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 import java.util.function.Function;
 
-@Slf4j
-final public class CreationGenerator implements Generator {
-    private final ValuedGenerator<String> valuedGenerator;
-    private final Function<Context, ResultContextManager> managerCreator;
+final public class CreationGenerator extends BaseGenerator {
     private final Deque<Class<? extends LongKeyJsonObj<?>>> types;
-
-    private Boolean fieldValidity;
 
     public static Builder builder(){
         return new Builder();
     }
 
-    private CreationGenerator(ValuedGenerator<String> valuedGenerator,
+    private CreationGenerator(Valued<String> key,
+                              ValuedGenerator<String> valuedGenerator,
                               Function<Context, ResultContextManager> managerCreator,
                               List<Class<? extends LongKeyJsonObj<?>>> types) {
-        this.valuedGenerator = valuedGenerator;
-        this.managerCreator = managerCreator;
+        super(key, valuedGenerator, managerCreator);
         this.types = new ArrayDeque<>(types);
+
+        this.fieldValidator
+                .caller(getClass().getSimpleName());
     }
 
     @Override
-    public Optional<Seed> getNextIfExist(Context context) {
-        if (checkFields()){
-            Class<? extends LongKeyJsonObj<?>> type = types.pollFirst();
-            if (type != null){
-                Seed seed = SeedImpl.builder()
-                        .type(CreationTask.class)
-                        .field("managerCreator", managerCreator)
-                        .field("valuedGenerator", valuedGenerator)
-                        .field("type", type)
-                        .build();
-                return Optional.of(seed);
-            }
+    protected Optional<Seed> getNext(Context context) {
+        Class<? extends LongKeyJsonObj<?>> type = types.pollFirst();
+        if (type != null){
+            Seed seed = SeedImpl.builder()
+                    .type(CreationTask.class)
+                    .field("managerCreator", managerCreator)
+                    .field("valuedGenerator", valuedGenerator)
+                    .field("key", key)
+                    .field("type", type)
+                    .build();
+            return Optional.of(seed);
         }
         return Optional.empty();
     }
 
-    private boolean checkFields() {
-        if (fieldValidity == null){
-            if (managerCreator == null){
-                log.warn("Manager creator is null");
-                fieldValidity = false;
-            } else if (valuedGenerator == null){
-                log.warn("Valued generator is null");
-                fieldValidity = false;
-            } else {
-                fieldValidity = true;
-            }
-        }
-        return fieldValidity;
-    }
-
-    public static class Builder {
+    public static class Builder extends BaseBuilder{
         private final List<Class<? extends LongKeyJsonObj<?>>> types = new ArrayList<>();
-
-        private ValuedGenerator<String> valuedGenerator;
-        private Function<Context, ResultContextManager> managerCreator;
-
-        public Builder managerCreator(Function<Context, ResultContextManager> managerCreator){
-            this.managerCreator = managerCreator;
-            return this;
-        }
-
-        public Builder valuedGenerator(ValuedGenerator<String> valuedGenerator){
-            this.valuedGenerator = valuedGenerator;
-            return this;
-        }
-
-        public CreationGenerator build(){
-            return new CreationGenerator(valuedGenerator, managerCreator, types);
-        }
 
         public Builder type(Class<? extends LongKeyJsonObj<?>> type) {
             types.add(type);
             return this;
+        }
+
+        public CreationGenerator build(){
+            return new CreationGenerator(key, valuedGenerator, managerCreator, types);
         }
     }
 }
--- src/test/java/kpn/financecontroller/initialization/generators/seed/CreationGeneratorTest.java
+++ src/test/java/kpn/financecontroller/initialization/generators/seed/CreationGeneratorTest.java
@@ -1,13 +1,16 @@
 package kpn.financecontroller.initialization.generators.seed;
 
+import kpn.financecontroller.initialization.generators.valued.Valued;
 import kpn.financecontroller.initialization.generators.valued.ValuedGenerator;
 import kpn.financecontroller.initialization.generators.valued.ValuedStringGenerator;
 import kpn.financecontroller.initialization.jsonObjs.LongKeyJsonObj;
 import kpn.financecontroller.initialization.managers.context.ResultContextManager;
 import kpn.financecontroller.initialization.tasks.CreationTask;
+import kpn.financecontroller.initialization.tasks.testUtils.TestKeys;
 import kpn.financecontroller.initialization.tasks.testUtils.TestManagerCreator;
 import kpn.taskexecutor.lib.contexts.Context;
 import kpn.taskexecutor.lib.contexts.SimpleContext;
+import kpn.taskexecutor.lib.generators.Generator;
 import kpn.taskexecutor.lib.seeds.Seed;
 import org.junit.jupiter.api.Test;
 
@@ -21,18 +24,30 @@ public class CreationGeneratorTest {
     private static final Context CONTEXT = new SimpleContext();
     private static final Function<Context, ResultContextManager> CREATOR = new TestManagerCreator();
     private static final ValuedGenerator<String> VALUED_GENERATOR = new ValuedStringGenerator();
+    private static final Valued<String> KEY = TestKeys.KEY;
 
     @Test
     void shouldCheckNextGetting_ifManagerCreatorNull() {
-        CreationGenerator seedGenerator = CreationGenerator.builder().build();
+        Generator seedGenerator = CreationGenerator.builder().build();
         Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
         assertThat(maybeSeed).isEmpty();
     }
 
     @Test
     void shouldCheckNextGetting_ifValuedGeneratorNull() {
-        CreationGenerator seedGenerator = CreationGenerator.builder()
+        Generator seedGenerator = CreationGenerator.builder()
                 .managerCreator(CREATOR)
+                .build();
+
+        Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
+        assertThat(maybeSeed).isEmpty();
+    }
+
+    @Test
+    void shouldCheckNextGetting_ifKeyNull() {
+        Generator seedGenerator = CreationGenerator.builder()
+                .managerCreator(CREATOR)
+                .valuedGenerator(VALUED_GENERATOR)
                 .build();
 
         Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
@@ -44,13 +59,15 @@ public class CreationGeneratorTest {
         Map<String, Object> expectedFields = Map.of(
                 "valuedGenerator", VALUED_GENERATOR,
                 "managerCreator", CREATOR,
+                "key", KEY,
                 "type", TestJsonObj.class
         );
 
-        CreationGenerator seedGenerator = CreationGenerator.builder()
+        Generator seedGenerator = CreationGenerator.builder()
+                .type(TestJsonObj.class)
                 .managerCreator(CREATOR)
                 .valuedGenerator(VALUED_GENERATOR)
-                .type(TestJsonObj.class)
+                .key(KEY)
                 .build();
 
         Optional<Seed> maybeSeed = seedGenerator.getNextIfExist(CONTEXT);
