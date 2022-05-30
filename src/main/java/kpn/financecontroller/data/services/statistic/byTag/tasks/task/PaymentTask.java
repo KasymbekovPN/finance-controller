340bb6e3b68f7ec909570834901badbca1282341
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-05-30 22:25:58 +0300

    fix

--- src/main/java/kpn/financecontroller/data/services/statistic/byTag/tasks/task/PaymentTask.java
+++ src/main/java/kpn/financecontroller/data/services/statistic/byTag/tasks/task/PaymentTask.java
@@ -1,8 +1,10 @@
 package kpn.financecontroller.data.services.statistic.byTag.tasks.task;
 
+import kpn.financecontroller.data.domains.product.Product;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -14,6 +16,7 @@ final public class PaymentTask implements Task{
     private LocalDate beginTime;
     private boolean endTimeEnable;
     private LocalDate endTime;
+    private Set<Product> products;
 
     public static PaymentTask copy(PaymentTask t){
         PaymentTask task = new PaymentTask();
@@ -21,6 +24,7 @@ final public class PaymentTask implements Task{
         task.setBeginTime(t.getBeginTime());
         task.setEndTimeEnable(t.isEndTimeEnable());
         task.setEndTime(t.getEndTime());
+        task.setProducts(t.getProducts());
         return task;
     }
 }
