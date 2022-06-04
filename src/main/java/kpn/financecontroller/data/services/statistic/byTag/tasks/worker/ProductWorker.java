77c3e5d284988a755b26d6e0cc95ad6a72244c4b
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-06-04 16:52:43 +0300

    ProductWorker is refactored

--- src/main/java/kpn/financecontroller/data/services/statistic/byTag/tasks/worker/ProductWorker.java
+++ src/main/java/kpn/financecontroller/data/services/statistic/byTag/tasks/worker/ProductWorker.java
@@ -1,10 +1,8 @@
 package kpn.financecontroller.data.services.statistic.byTag.tasks.worker;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.Predicate;
 import kpn.financecontroller.data.domains.product.Product;
-import kpn.financecontroller.data.domains.tag.Tag;
 import kpn.financecontroller.data.entities.product.ProductEntity;
-import kpn.financecontroller.data.entities.tag.QTagEntity;
 import kpn.financecontroller.data.services.dto.DTOService;
 import kpn.financecontroller.data.services.statistic.byTag.tasks.task.ProductTask;
 import kpn.lib.result.Result;
@@ -12,24 +10,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 @Component
 @AllArgsConstructor
 final public class ProductWorker implements Worker<ProductTask, Product> {
-
     private final DTOService<Product, ProductEntity> service;
-    // TODO: 02.06.2022 add task to expression converter
+    private final Function<ProductTask, Predicate> converter;
 
     @Override
     public Result<List<Product>> execute(ProductTask task) {
-        if (task.isAllTags()){
-            return service.loader().all();
-        }
-
-        Set<Long> tagIds = task.getTags().stream().map(Tag::getId).collect(Collectors.toSet());
-        BooleanExpression expression = QTagEntity.tagEntity.id.in(tagIds);
-        return service.executor().execute(expression);
+        return  task.isAllTags() ? service.loader().all() : service.executor().execute(converter.apply(task));
     }
 }
--- src/test/java/kpn/financecontroller/data/services/statistic/byTag/tasks/worker/ProductWorkerTest.java
+++ src/test/java/kpn/financecontroller/data/services/statistic/byTag/tasks/worker/ProductWorkerTest.java
@@ -4,6 +4,8 @@ import com.querydsl.core.types.Predicate;
 import kpn.financecontroller.data.domains.product.Product;
 import kpn.financecontroller.data.domains.tag.Tag;
 import kpn.financecontroller.data.entities.product.ProductEntity;
+import kpn.financecontroller.data.entities.product.QProductEntity;
+import kpn.financecontroller.data.entities.tag.TagEntity;
 import kpn.financecontroller.data.services.dto.DTOService;
 import kpn.financecontroller.data.services.dto.DTOServiceImpl;
 import kpn.financecontroller.data.services.dto.executors.PredicateExecutor;
@@ -17,6 +19,7 @@ import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,7 +47,7 @@ class ProductWorkerTest {
     void shouldCheckFinding_byTags() {
         ProductTask task = createTask(false, 1L, 2L);
 
-        ProductWorker worker = new ProductWorker(createService());
+        ProductWorker worker = new ProductWorker(createService(), createConverter());
         Result<List<Product>> result = worker.execute(task);
 
         assertThat(executorResult).isEqualTo(result);
@@ -54,7 +57,7 @@ class ProductWorkerTest {
     void shouldCheckFinding_forAll() {
         ProductTask task = createTask(true, 1L, 2L);
 
-        ProductWorker worker = new ProductWorker(createService());
+        ProductWorker worker = new ProductWorker(createService(), createConverter());
         Result<List<Product>> result = worker.execute(task);
 
         assertThat(loaderResult).isEqualTo(result);
@@ -78,6 +81,14 @@ class ProductWorkerTest {
         return new DTOServiceImpl<>(null, createLoader(), null, createExecutor());
     }
 
+    private Function<ProductTask, Predicate> createConverter() {
+        TestConverter converter = Mockito.mock(TestConverter.class);
+        Mockito
+                .when(converter.apply(Mockito.anyObject()))
+                .thenReturn(QProductEntity.productEntity.tagEntities.contains(new TagEntity()));
+        return converter;
+    }
+
     private PredicateExecutor<Product, Predicate> createExecutor() {
         TestExecutor executor = Mockito.mock(TestExecutor.class);
         Mockito
@@ -96,4 +107,5 @@ class ProductWorkerTest {
 
     abstract private static class TestExecutor implements PredicateExecutor<Product, Predicate> {}
     abstract private static class TestLoader implements Loader<Product, ProductEntity, Long>{}
+    abstract private static class TestConverter implements Function<ProductTask, Predicate>{}
 }
