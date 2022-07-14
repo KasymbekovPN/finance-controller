1e5f8707ad671b57d3c0944ea41f77b705be91ed
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-07-14 22:51:25 +0300

    ByIdDeletingExecutorImpl is done

---
+++ src/main/java/kpn/financecontroller/data/services/dto/deleters/ByIdDeletingExecutorImpl.java
@@ -0,0 +1,25 @@
+package kpn.financecontroller.data.services.dto.deleters;
+
+import kpn.lib.domain.Domain;
+import kpn.lib.entity.Entity;
+import kpn.lib.exception.DTOException;
+import kpn.lib.executor.DefaultExecutorResult;
+import kpn.lib.executor.ExecutorResult;
+import kpn.lib.executor.deleting.ByIdDeletingExecutor;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@AllArgsConstructor
+public class ByIdDeletingExecutorImpl<D extends Domain<Long>, E extends Entity<Long>> implements ByIdDeletingExecutor<Long, D> {
+    private final JpaRepository<E, Long> repository;
+
+    @Override
+    public ExecutorResult<D> delete(Long id) throws DTOException {
+        try{
+            repository.deleteById(id);
+            return new DefaultExecutorResult<>();
+        } catch (Throwable t){
+            throw new DTOException("executor.deleting.byId.fail");
+        }
+    }
+}
