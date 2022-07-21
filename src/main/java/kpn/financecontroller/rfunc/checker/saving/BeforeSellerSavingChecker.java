09170664dc7d0be1c9b691720d0b6b9f91da4ca6
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-07-21 22:39:20 +0300

    BeforeSellerSavingChecker is changed

---
+++ src/main/java/kpn/financecontroller/rfunc/checker/saving/BeforeSellerSavingChecker.java
@@ -0,0 +1,19 @@
+package kpn.financecontroller.rfunc.checker.saving;
+
+import kpn.financecontroller.data.domains.seller.Seller;
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public final class BeforeSellerSavingChecker extends AbstractBeforeSavingChecker<Seller> {
+    @Override
+    public Result<List<Seller>> apply(Seller value) {
+        String name = value.getName();
+        return name == null || name.isEmpty()
+                ? ImmutableResult.<List<Seller>>fail("checking.domain.seller.name.isEmpty")
+                : ImmutableResult.<List<Seller>>ok(List.of(value));
+    }
+}
