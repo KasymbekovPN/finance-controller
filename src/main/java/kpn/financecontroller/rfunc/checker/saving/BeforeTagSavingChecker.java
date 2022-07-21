7a68336ff97a8043a4ff9ce7c18a892c61f183b5
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-07-21 22:48:17 +0300

    BeforeTagSavingChecker is changed

---
+++ src/main/java/kpn/financecontroller/rfunc/checker/saving/BeforeTagSavingChecker.java
@@ -0,0 +1,19 @@
+package kpn.financecontroller.rfunc.checker.saving;
+
+import kpn.financecontroller.data.domains.tag.Tag;
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public final class BeforeTagSavingChecker extends AbstractBeforeSavingChecker<Tag> {
+    @Override
+    public Result<List<Tag>> apply(Tag value) {
+        String name = value.getName();
+        return name == null || name.isEmpty()
+                ? ImmutableResult.<List<Tag>>fail("checking.domain.tag.name.isEmpty")
+                : ImmutableResult.<List<Tag>>ok(List.of(value));
+    }
+}
--- src/test/java/kpn/financecontroller/rfunc/checker/saving/BeforeTagSavingCheckerTest.java
+++ src/test/java/kpn/financecontroller/rfunc/checker/saving/BeforeTagSavingCheckerTest.java
@@ -1,41 +1,42 @@
-// TODO: 21.07.2022 restore
-//package kpn.financecontroller.rfunc.checker.saving;
-//
-//import kpn.financecontroller.data.domains.tag.Tag;
-//import kpn.lib.result.ImmutableResult;
-//import kpn.lib.result.Result;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class BeforeTagSavingCheckerTest {
-//
-//    @Test
-//    void shouldCheck_whenNameNull() {
-//        Tag tag = new Tag();
-//        ImmutableResult<Tag> expectedResult = ImmutableResult.<Tag>fail("checking.domain.tag.name.isEmpty");
-//
-//        Result<Tag> result = new BeforeTagSavingChecker().apply(tag);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    @Test
-//    void shouldCheck_whenNameEmpty() {
-//        Tag tag = new Tag();
-//        tag.setName("");
-//        ImmutableResult<Tag> expectedResult = ImmutableResult.<Tag>fail("checking.domain.tag.name.isEmpty");
-//
-//        Result<Tag> result = new BeforeTagSavingChecker().apply(tag);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    @Test
-//    void shouldCheck() {
-//        Tag tag = new Tag();
-//        tag.setName("some.name");
-//        ImmutableResult<Tag> expectedResult = ImmutableResult.<Tag>ok(tag);
-//
-//        Result<Tag> result = new BeforeTagSavingChecker().apply(tag);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//}
+package kpn.financecontroller.rfunc.checker.saving;
+
+import kpn.financecontroller.data.domains.tag.Tag;
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeforeTagSavingCheckerTest {
+
+    @Test
+    void shouldCheck_whenNameNull() {
+        Tag tag = new Tag();
+        ImmutableResult<List<Tag>> expectedResult = ImmutableResult.<List<Tag>>fail("checking.domain.tag.name.isEmpty");
+
+        Result<List<Tag>> result = new BeforeTagSavingChecker().apply(tag);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    void shouldCheck_whenNameEmpty() {
+        Tag tag = new Tag();
+        tag.setName("");
+        ImmutableResult<List<Tag>> expectedResult = ImmutableResult.<List<Tag>>fail("checking.domain.tag.name.isEmpty");
+
+        Result<List<Tag>> result = new BeforeTagSavingChecker().apply(tag);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    void shouldCheck() {
+        Tag tag = new Tag();
+        tag.setName("some.name");
+        ImmutableResult<List<Tag>> expectedResult = ImmutableResult.<List<Tag>>ok(List.of(tag));
+
+        Result<List<Tag>> result = new BeforeTagSavingChecker().apply(tag);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+}
