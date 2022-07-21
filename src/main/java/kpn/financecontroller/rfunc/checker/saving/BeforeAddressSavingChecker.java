76abab9440e7bc6f379469682c2401750d34162e
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-07-21 22:03:59 +0300

    BeforeAddressSavingChecker is changed

---
+++ src/main/java/kpn/financecontroller/rfunc/checker/saving/BeforeAddressSavingChecker.java
@@ -0,0 +1,23 @@
+package kpn.financecontroller.rfunc.checker.saving;
+
+import kpn.financecontroller.data.domains.address.Address;
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public final class BeforeAddressSavingChecker extends AbstractBeforeSavingChecker<Address> {
+
+    @Override
+    public Result<List<Address>> apply(Address value) {
+        String name = value.getName();
+        if (name == null || name.isEmpty()){
+            return ImmutableResult.<List<Address>>fail("checking.domain.address.name.isEmpty");
+        }
+        return value.getStreet() == null
+                ? ImmutableResult.<List<Address>>fail("checking.domain.address.street.isEmpty")
+                : ImmutableResult.<List<Address>>ok(List.of(value));
+    }
+}
--- src/test/java/kpn/financecontroller/rfunc/checker/saving/BeforeAddressSavingCheckerTest.java
+++ src/test/java/kpn/financecontroller/rfunc/checker/saving/BeforeAddressSavingCheckerTest.java
@@ -1,71 +1,72 @@
-// TODO: 21.07.2022 restore
-//package kpn.financecontroller.rfunc.checker.saving;
-//
-//import kpn.financecontroller.data.domains.address.Address;
-//import kpn.financecontroller.data.domains.street.Street;
-//import kpn.lib.result.ImmutableResult;
-//import kpn.lib.result.Result;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//class BeforeAddressSavingCheckerTest {
-//
-//    @Test
-//    void shouldCheck_whenNameNull() {
-//        Address domain = new Builder().build();
-//        ImmutableResult<Address> expectedResult = ImmutableResult.<Address>fail("checking.domain.address.name.isEmpty");
-//
-//        Result<Address> result = new BeforeAddressSavingChecker().apply(domain);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    @Test
-//    void shouldCheck_whenNameEmpty() {
-//        Address domain = new Builder().name("").build();
-//        ImmutableResult<Address> expectedResult = ImmutableResult.<Address>fail("checking.domain.address.name.isEmpty");
-//
-//        Result<Address> result = new BeforeAddressSavingChecker().apply(domain);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    @Test
-//    void shouldCheck_whenStreetNull() {
-//        Address domain = new Builder().name("name").build();
-//        ImmutableResult<Address> expectedResult = ImmutableResult.<Address>fail("checking.domain.address.street.isEmpty");
-//
-//        Result<Address> result = new BeforeAddressSavingChecker().apply(domain);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    @Test
-//    void shouldCheck() {
-//        Address domain = new Builder().name("name").street(new Street()).build();
-//        ImmutableResult<Address> expectedResult = ImmutableResult.<Address>ok(domain);
-//
-//        Result<Address> result = new BeforeAddressSavingChecker().apply(domain);
-//        assertThat(expectedResult).isEqualTo(result);
-//    }
-//
-//    private static class Builder{
-//        private String name;
-//        private Street street;
-//
-//        public Address build() {
-//            Address address = new Address();
-//            address.setName(name);
-//            address.setStreet(street);
-//            return address;
-//        }
-//
-//        public Builder name(String name) {
-//            this.name = name;
-//            return this;
-//        }
-//
-//        public Builder street(Street street) {
-//            this.street = street;
-//            return this;
-//        }
-//    }
-//}
+package kpn.financecontroller.rfunc.checker.saving;
+
+import kpn.financecontroller.data.domains.address.Address;
+import kpn.financecontroller.data.domains.street.Street;
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeforeAddressSavingCheckerTest {
+
+    @Test
+    void shouldCheck_whenNameNull() {
+        Address domain = new Builder().build();
+        ImmutableResult<List<Address>> expectedResult = ImmutableResult.<List<Address>>fail("checking.domain.address.name.isEmpty");
+
+        Result<List<Address>> result = new BeforeAddressSavingChecker().apply(domain);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    void shouldCheck_whenNameEmpty() {
+        Address domain = new Builder().name("").build();
+        ImmutableResult<List<Address>> expectedResult = ImmutableResult.<List<Address>>fail("checking.domain.address.name.isEmpty");
+
+        Result<List<Address>> result = new BeforeAddressSavingChecker().apply(domain);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    void shouldCheck_whenStreetNull() {
+        Address domain = new Builder().name("name").build();
+        ImmutableResult<List<Address>> expectedResult = ImmutableResult.<List<Address>>fail("checking.domain.address.street.isEmpty");
+
+        Result<List<Address>> result = new BeforeAddressSavingChecker().apply(domain);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    void shouldCheck() {
+        Address domain = new Builder().name("name").street(new Street()).build();
+        ImmutableResult<List<Address>> expectedResult = ImmutableResult.<List<Address>>ok(List.of(domain));
+
+        Result<List<Address>> result = new BeforeAddressSavingChecker().apply(domain);
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    private static class Builder{
+        private String name;
+        private Street street;
+
+        public Address build() {
+            Address address = new Address();
+            address.setName(name);
+            address.setStreet(street);
+            return address;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder street(Street street) {
+            this.street = street;
+            return this;
+        }
+    }
+}
