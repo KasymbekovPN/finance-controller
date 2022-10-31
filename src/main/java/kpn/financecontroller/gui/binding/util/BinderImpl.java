78ab62ea71c790f9e99281ab251b7cb193a93b02
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-10-31 17:01:19 +0300

    refactoring

--- src/main/java/kpn/financecontroller/gui/binding/util/BinderImpl.java
+++ src/main/java/kpn/financecontroller/gui/binding/util/BinderImpl.java
@@ -4,50 +4,63 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-// TODO: 22.10.2022 change monitor - not this but Object with name lock
 public class BinderImpl<K, V> implements Binder<K, V> {
     private final Map<K, V> map = new HashMap<>();
 
-    @Override
-    public synchronized boolean bind(K k, V v) {
-        if (map.containsKey(k)){
-            return false;
-        }
-        map.put(k, v);
-        return true;
-    }
+    private final Object lock = new Object();
 
     @Override
-    public synchronized boolean unbind(K k) {
-        if (map.containsKey(k)){
-            map.remove(k);
+    public boolean bind(K k, V v) {
+        synchronized (lock){
+            if (map.containsKey(k)){
+                return false;
+            }
+            map.put(k, v);
             return true;
         }
-        return false;
     }
 
     @Override
-    public synchronized void changeBinding(K k, V v) {
-        if (map.containsKey(k)){
-            unbind(k);
+    public boolean unbind(K k) {
+        synchronized (lock){
+            if (map.containsKey(k)){
+                map.remove(k);
+                return true;
+            }
+            return false;
         }
-        bind(k, v);
     }
 
     @Override
-    public synchronized boolean isBound(K k) {
-        return map.containsKey(k);
+    public void changeBinding(K k, V v) {
+        synchronized (lock){
+            if (map.containsKey(k)){
+                unbind(k);
+            }
+            bind(k, v);
+        }
     }
 
     @Override
-    public synchronized boolean isBound(K k, V v) {
-        return isBound(k) && map.get(k).equals(v);
+    public boolean isBound(K k) {
+        synchronized (lock){
+            return map.containsKey(k);
+        }
     }
 
     @Override
-    public synchronized Optional<V> get(K k) {
-        return map.containsKey(k)
-                ? Optional.of(map.get(k))
-                : Optional.empty();
+    public boolean isBound(K k, V v) {
+        synchronized (lock){
+            return isBound(k) && map.get(k).equals(v);
+        }
+    }
+
+    @Override
+    public Optional<V> get(K k) {
+        synchronized (lock){
+            return map.containsKey(k)
+                    ? Optional.of(map.get(k))
+                    : Optional.empty();
+        }
     }
 }
