edcfa95e584fbe2e47f12fba955703ecd11ed885
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-01-03 17:56:47 +0300

    ResourceFileReader is done

---
+++ backend/ctrlf/src/main/java/kpn/ctrlf/client/i18n/reader/ResourceFileReader.java
@@ -0,0 +1,26 @@
+package kpn.ctrlf.client.i18n.reader;
+
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.function.Function;
+
+public final class ResourceFileReader implements Function<String, Result<String>> {
+
+	@Override
+	public Result<String> apply(String path) {
+		try {
+			File file = new ClassPathResource(path).getFile();
+			return ImmutableResult.<String>ok(
+				new String(Files.readAllBytes(file.toPath()))
+			);
+		} catch (Throwable e) {
+			return ImmutableResult.<String>bFail("resource-file-reader.exception")
+				.arg(e.getMessage())
+				.build();
+		}
+	}
+}
---
+++ backend/ctrlf/src/test/java/kpn/ctrlf/client/i18n/reader/ResourceFileReaderTest.java
@@ -0,0 +1,47 @@
+package kpn.ctrlf.client.i18n.reader;
+
+import kpn.lib.result.ImmutableResult;
+import kpn.lib.result.Result;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ResourceFileReaderTest {
+
+	private static final String CODE_ON_EXCEPTION = "resource-file-reader.exception";
+
+	@Test
+	void shouldCheckReading_exception() {
+		ResourceFileReader reader = new ResourceFileReader();
+		Result<String> result = reader.apply(null);
+
+		assertThat(result).isNotNull();
+		assertThat(result.isSuccess()).isFalse();
+		assertThat(result.getSeed().getCode()).isEqualTo(CODE_ON_EXCEPTION);
+	}
+
+	@Test
+	void shouldCheckReading_fileDoesNotExist() {
+		String path = getCalculateFilePath("nonExist.txt");
+
+		Result<String> result = new ResourceFileReader().apply(path);
+		assertThat(result).isNotNull();
+		assertThat(result.isSuccess()).isFalse();
+		assertThat(result.getSeed().getCode()).isEqualTo(CODE_ON_EXCEPTION);
+	}
+
+	@Test
+	void shouldCheckReading() {
+		ImmutableResult<String> expectedResult = ImmutableResult.<String>ok("content\n");
+
+		String path = getCalculateFilePath("exist.txt");
+
+		Result<String> result = new ResourceFileReader().apply(path);
+		assertThat(result).isEqualTo(expectedResult);
+	}
+
+	private String getCalculateFilePath(String fileName){
+		String packageName = getClass().getPackageName();
+		return "/" + packageName.replace(".", "/") + "/" + fileName;
+	}
+}
