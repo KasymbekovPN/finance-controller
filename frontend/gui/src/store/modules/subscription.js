7d3bc475bfc3e8b56d312d15f57476775df6d477
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-01-08 17:59:15 +0300

    SUBSCRIPTION_AUTH_REQUEST is done

--- frontend/gui/src/store/modules/subscription.js
+++ frontend/gui/src/store/modules/subscription.js
@@ -1,7 +1,12 @@
+import { AUTH_LOGIN_RESPONSE } from "../actions/auth";
 import { CLIENT_PARAMS_SET } from "../actions/clientParams";
 import { CONNECTION_SEND } from "../actions/connection";
 import { I18N_INIT } from "../actions/i18n";
-import { SUBSCRIPTION_CLIENT_PARAMS, SUBSCRIPTION_I18N } from "../actions/subscription";
+import {
+	SUBSCRIPTION_CLIENT_PARAMS,
+	SUBSCRIPTION_I18N,
+	SUBSCRIPTION_AUTH_REQUEST
+} from "../actions/subscription";
 
 const state = {};
 
@@ -20,6 +25,10 @@ const actions = {
 	[SUBSCRIPTION_I18N]: ({dispatch}, response) => {
 		const data = JSON.parse(response.body);
 		dispatch(I18N_INIT, {data});
+	},
+	[SUBSCRIPTION_AUTH_REQUEST]: ({dispatch}, response) => {
+		const data = JSON.parse(response.body);
+		dispatch(AUTH_LOGIN_RESPONSE, data);
 	}
 };
 
