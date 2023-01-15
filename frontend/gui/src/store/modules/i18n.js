e22b0490facda40bd003f283d765e2749266ea2e
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-01-15 17:32:18 +0300

    i18n-store is changed

--- frontend/gui/src/store/modules/i18n.js
+++ frontend/gui/src/store/modules/i18n.js
@@ -1,7 +1,7 @@
-import { TemplateEngine } from "../../utils/templateEngine";
-import I18n from "../../i18n/i18n";
-import { createTrTemplates,  } from "../../i18n/trTemplates";
 import { I18N_SET_LOCALE, I18N_SET_TEMPLATES } from "../sconst/i18n";
+import { getTranslation } from "../imps/i18n-getters";
+import { setI18nLocale, setI18nTemplates } from "../imps/i18n-actions";
+import { mutateOnSetLocale, mutateOnSetTemplates } from "../imps/i18n-mutations";
 
 const state = {
 	i18n: undefined,
@@ -9,38 +9,17 @@ const state = {
 };
 
 const getters = {
-	translate: state => (code, args) => {
-		if (state.locale != undefined && state.i18n !== undefined){
-			const template = state.i18n.getTemplate(code, state.locale);
-			return new TemplateEngine().execute(template, args);
-		}
-		return code;
-	}
+	translate: state => (code, args) => { getTranslation(state, code, args); }
 };
 
 const actions = {
-	[I18N_SET_LOCALE]: ({commit}, locale) => {
-		commit(I18N_SET_LOCALE, locale);
-	},
-	[I18N_SET_TEMPLATES]: ({commit}, data) => {
-		commit(I18N_SET_TEMPLATES, data);
-	}
+	[I18N_SET_LOCALE]: setI18nLocale,
+	[I18N_SET_TEMPLATES]: setI18nTemplates
 };
 
 const mutations = {
-	[I18N_SET_LOCALE]: (state, locale) => {
-		state.locale = locale;
-	},
-	[I18N_SET_TEMPLATES]: (state, data) => {
-		const i18n = new I18n();
-		for(const code in data.templates){
-			const result = createTrTemplates(code, data.templates[code]);
-			if (result.success){
-				i18n.addTemplate(result.value);
-			}
-		}
-		state.i18n = i18n;
-	}
+	[I18N_SET_LOCALE]: mutateOnSetLocale,
+	[I18N_SET_TEMPLATES]: mutateOnSetTemplates
 };
 
 export default {
