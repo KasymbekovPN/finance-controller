30b141e24f11af0473ad5043ab3cd691c5fdc3b1
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-01-15 16:38:39 +0300

    subscription-store is changed

--- frontend/gui/src/store/modules/subscription.js
+++ frontend/gui/src/store/modules/subscription.js
@@ -1,6 +1,8 @@
-import { AUTH_LOGIN_RESPONSE } from "../sconst/auth";
-import { CONNECTION_SEND } from "../sconst/connection";
-import { I18N_SET_LOCALE, I18N_SET_TEMPLATES } from "../sconst/i18n";
+import {
+	processAuthRequestSubscription,
+	processClientParamsSubscription,
+	processI18nSubscription
+} from "../imps/subscription-actions";
 import {
 	SUBSCRIPTION_CLIENT_PARAMS,
 	SUBSCRIPTION_I18N,
@@ -12,23 +14,9 @@ const state = {};
 const getters = {};
 
 const actions = {
-	[SUBSCRIPTION_CLIENT_PARAMS]: ({dispatch}, response) => {
-		const clientParams = JSON.parse(response.body);
-		dispatch(I18N_SET_LOCALE, clientParams.locale);
-		dispatch(CONNECTION_SEND, {
-			destination: '/i18nRequest',
-			headers: {},
-			body: {}
-		});
-	},
-	[SUBSCRIPTION_I18N]: ({dispatch}, response) => {
-		const data = JSON.parse(response.body);
-		dispatch(I18N_SET_TEMPLATES, data);
-	},
-	[SUBSCRIPTION_AUTH_REQUEST]: ({dispatch}, response) => {
-		const data = JSON.parse(response.body);
-		dispatch(AUTH_LOGIN_RESPONSE, data);
-	}
+	[SUBSCRIPTION_CLIENT_PARAMS]: processClientParamsSubscription,
+	[SUBSCRIPTION_I18N]: processI18nSubscription,
+	[SUBSCRIPTION_AUTH_REQUEST]: processAuthRequestSubscription
 };
 
 const mutations = {};
